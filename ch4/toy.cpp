51a2667789b4ffdcc0a4f898061cbea004b3ee23
remysys <109655176+remysys@users.noreply.github.com>
2023-02-27 04:34:17 +0800

    feat(Kaleidoscope): print llvm ir

--- ch4/toy.cpp
+++ ch4/toy.cpp
@@ -304,52 +304,40 @@ static std::unique_ptr<ExprAST> ParsePrimary() {
   }
 }
 
-/// binoprhs
-///   ::= ('+' primary)*
-static std::unique_ptr<ExprAST> ParseBinOpRHS(int ExprPrec,
-                                              std::unique_ptr<ExprAST> LHS) {
-  // If this is a binop, find its precedence.
-  while (true) {
-    int TokPrec = GetTokPrecedence();
+/// RecursiveParseExpression
 
-    // If this is a binop that binds at least as tightly as the current binop,
-    // consume it, otherwise we are done.
-    if (TokPrec < ExprPrec)
-      return LHS;
-
-    // Okay, we know this is a binop.
-    int BinOp = CurTok;
-    getNextToken(); // eat binop
-
-    // Parse the primary expression after the binary operator.
-    auto RHS = ParsePrimary();
-    if (!RHS)
-      return nullptr;
-
-    // If BinOp binds less tightly with RHS than the operator after RHS, let
-    // the pending operator take RHS as its LHS.
-    int NextPrec = GetTokPrecedence();
-    if (TokPrec < NextPrec) {
-      RHS = ParseBinOpRHS(TokPrec + 1, std::move(RHS));
-      if (!RHS)
-        return nullptr;
-    }
-
-    // Merge LHS/RHS.
-    LHS =
-        std::make_unique<BinaryExprAST>(BinOp, std::move(LHS), std::move(RHS));
-  }
-}
-
-/// expression
-///   ::= primary binoprhs
-///
-static std::unique_ptr<ExprAST> ParseExpression() {
+static std::unique_ptr<ExprAST> RecursiveParseExpression(int ExprPrec) {
   auto LHS = ParsePrimary();
   if (!LHS)
     return nullptr;
+  
+  int TokPrec = GetTokPrecedence();
+  if (TokPrec < 0) {
+    // -1 if the token is not a binary operator
+    return LHS;
+  }
 
-  return ParseBinOpRHS(0, std::move(LHS));
+  while (ExprPrec < TokPrec) {
+    int BinOp = CurTok;
+    getNextToken(); // eat binop
+
+    auto RHS = RecursiveParseExpression(TokPrec);
+    if (!RHS) {
+      return nullptr;
+    }
+
+    LHS = std::make_unique<BinaryExprAST>(BinOp, std::move(LHS), std::move(RHS));
+
+    TokPrec = GetTokPrecedence();
+  }
+
+  return LHS;
+}
+
+/// expression
+
+static std::unique_ptr<ExprAST> ParseExpression() {
+  return RecursiveParseExpression(0);
 }
 
 /// prototype
@@ -426,7 +414,7 @@ Function *getFunction(std::string Name) {
   // First, see if the function has already been added to the current module.
   if (auto *F = TheModule->getFunction(Name))
     return F;
-
+  
   // If not, check whether we can codegen the declaration from some existing
   // prototype.
   auto FI = FunctionProtos.find(Name);
@@ -600,7 +588,11 @@ static void HandleExtern() {
 static void HandleTopLevelExpression() {
   // Evaluate a top-level expression into an anonymous function.
   if (auto FnAST = ParseTopLevelExpr()) {
-    if (FnAST->codegen()) {
+    if (auto *FnIR = FnAST->codegen()) {
+      fprintf(stderr, "Read top-level expression:");
+      FnIR->print(errs());
+      fprintf(stderr, "\n");
+
       // JIT the module containing the anonymous expression, keeping a handle so
       // we can free it later.
       auto H = TheJIT->addModule(std::move(TheModule));
